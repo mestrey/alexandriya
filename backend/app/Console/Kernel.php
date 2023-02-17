99bd617403d78d5c5218e1d05321804b92d67f28
ymestre <ymestre@team.amocrm.com>
2023-02-17 17:55:05 +0300

    [Backend] Adding command for secrets

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -7,7 +7,9 @@ use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [];
+    protected $commands = [
+        \App\Console\Commands\GenerateSecretCommand::class,
+    ];
 
     protected function schedule(Schedule $schedule)
     {
